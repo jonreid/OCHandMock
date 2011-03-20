c00964b634f78a135fb4bc26f5ebaa1eb0346e2f
Jon Reid <jon.reid@mac.com>
2011-03-20 12:04:39 -0700

    Fixed verifications to do nothing if there are no expectations. Expose HMExpectationMatcher actual argument property so tests can mine it. Added uber-header.

---
+++ OCHandMock/OCHandMock.h
@@ -0,0 +1,8 @@
+//
+//  OCHandMock - OCHandMock.h
+//  Copyright 2011 Jonathan M. Reid. See LICENSE.txt
+//
+
+#import "HMExpectationCounter.h"
+#import "HMExpectationMatcher.h"
+#import "HMVerifier.h"
